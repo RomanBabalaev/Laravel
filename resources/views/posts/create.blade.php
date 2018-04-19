c11b6dfd0ab00aa9a230801fc7a9ce0de3e93377
Roman <87jormen@gmail.com>
2018-04-18 23:43:50 -0400

    blade done

--- resources/views/posts/create.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="/posts/store" method="POST">
-    <input type="text" name="title"> <br>
-    <input type="text" name="content"><br>
-    <input type="text" name="user_id"><br>
-    <input type="submit">
-</form>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index')->with('title', 'ГеймсМаркет - Главная');
 });
-Route::get('/posts/populate', 'PostController@populate');
-Route::get('/posts/', 'PostController@index');
-Route::get('/posts/crate', 'PostController@create');
+Route::get('/about', function () {
+    return view('about')->with('title', 'ГеймсМаркет - О компании');
+});
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
